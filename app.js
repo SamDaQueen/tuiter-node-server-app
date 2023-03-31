ba7c7bfad2779d11886afde77017fa3a103333e3
SamDaQueen <samreenreyaz@outlook.com>
2023-03-31 01:18:41 -0400

    CRUD tuit controller

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-
-import express from 'express'
+import express from "express";
+import cors from "cors";
 import HelloController from "./controllers/hello-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
-const app = express()
-app.use(express.json())
 
-HelloController(app)
-UsersController(app)
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.listen(4000)
+TuitsController(app);
+HelloController(app);
+UsersController(app);
+
+app.listen(process.env.POST || 4000);
